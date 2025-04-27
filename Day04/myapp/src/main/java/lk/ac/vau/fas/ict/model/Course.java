fdf578a89cad47b63b27e875ba2847dbf9ced1dc
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2025-04-27 07:55:45 +0000

    create course model

---
+++ Day04/myapp/src/main/java/lk/ac/vau/fas/ict/model/Course.java
@@ -0,0 +1,26 @@
+package lk.ac.vau.fas.ict.model;
+
+public class Course {
+	  private int CourseId;
+	  private String CoursName;
+	public Course(int courseId, String coursName) {
+		super();
+		CourseId = courseId;
+		CoursName = coursName;
+	}
+	public int getCourseId() {
+		return CourseId;
+	}
+	public void setCourseId(int courseId) {
+		CourseId = courseId;
+	}
+	public String getCoursName() {
+		return CoursName;
+	}
+	public void setCoursName(String coursName) {
+		CoursName = coursName;
+	}
+	  
+	  
+
+}
